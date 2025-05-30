b47473f0a53142f522580a4137e6bda463b259f8
beni25-mak <benimakumbu24@gmail.com>
2025-05-30 15:22:07 +0100

    modification de la source  des données

--- module2/quali.py
+++ module2/quali.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np  
 import matplotlib.pyplot as plt
-matplotlib
+
 st.title(" :red[Anlayse des données qualitative]")
 #st.subheader(":blue[Auteur] :blue[M. Beni Nzimba]")
 
